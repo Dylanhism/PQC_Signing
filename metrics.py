24442cb5b8a9ee9d316ed6a11f3308ec4bef1c86
Dylan H. <dylan.1451@hotmail.com>
2024-04-10 22:58:41 -0400

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -67,29 +67,29 @@ def graph():
         main.batch_verify('./test', 'haraka_256f')
         verify_times[5][i] = time.time() - startTime
 
-        # startTime = time.time()
-        # main.batch_process('./test', 'sha2_128f')
-        # process_times[6][i] = time.time() - startTime
+        startTime = time.time()
+        main.batch_process('./test', 'sha2_128f')
+        process_times[6][i] = time.time() - startTime
 
-        # startTime = time.time()
-        # main.batch_verify('./test', 'sha2_128f')
-        # verify_times[6][i] = time.time() - startTime
+        startTime = time.time()
+        main.batch_verify('./test', 'sha2_128f')
+        verify_times[6][i] = time.time() - startTime
 
-        # startTime = time.time()
-        # main.batch_process('./test', 'sha2_192f')
-        # process_times[7][i] = time.time() - startTime
+        startTime = time.time()
+        main.batch_process('./test', 'sha2_192f')
+        process_times[7][i] = time.time() - startTime
 
-        # startTime = time.time()
-        # main.batch_verify('./test', 'sha2_192f')
-        # verify_times[7][i] = time.time() - startTime
+        startTime = time.time()
+        main.batch_verify('./test', 'sha2_192f')
+        verify_times[7][i] = time.time() - startTime
 
-        # startTime = time.time()
-        # main.batch_process('./test', 'sha2_256f')
-        # process_times[8][i] = time.time() - startTime
+        startTime = time.time()
+        main.batch_process('./test', 'sha2_256f')
+        process_times[8][i] = time.time() - startTime
 
-        # startTime = time.time()
-        # main.batch_verify('./test', 'sha2_256f')
-        # verify_times[8][i] = time.time() - startTime
+        startTime = time.time()
+        main.batch_verify('./test', 'sha2_256f')
+        verify_times[8][i] = time.time() - startTime
 
     for i in range(nAlgs):
         processAvgs[i] = np.average(process_times[i])
@@ -98,17 +98,15 @@ def graph():
 
     labels= ['Shake128', 'Shake192', 'Shake256', 'Haraka128', 'Haraka192', 'Haraka256', 'Sha2-128', 'Sha2-192', 'Sha2-256']
 
-    plt.figure(figsize=(20, 20))
+    plt.figure(figsize=(10, 5))
     plt.bar(labels, processAvgs, color='red', width=0.7)
-    plt.set_xticklabels(labels, rotation=45)
     plt.ylabel("Time taken (Sec)")
     plt.title("Time vs Signing with SPHINCS+")
     plt.savefig('sign_plot.png')
     plt.close()
 
-    plt.figure(figsize=(20, 20))
+    plt.figure(figsize=(11, 5))
     plt.bar(labels, verifyAvgs, color='blue', width=0.7)
-    plt.set_xticklabels(labels, rotation=45)
     plt.ylabel("Time taken (Sec)")
     plt.title("Time vs Verifying with SPHINCS+")
     plt.savefig('verify_plot.png')
